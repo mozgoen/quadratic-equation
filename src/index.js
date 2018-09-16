529efbbc6315527d2d95c03f1326923dd115cba1
Katriaryna Mazgo <ekaterina.mozgo@mail.ru>
2018-09-16 21:09:14 +0300

    quadratic-equation/fix: implemented solveEquation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let strOfEquation = equation.replace(/ /g, "");
+    let arrOfEquation = strOfEquation.split("*x");
+    arrOfEquation[1] = arrOfEquation[1].substring(2);
+
+    for (let i = 0; i < arrOfEquation.length; i++) {
+        arrOfEquation[i] = Number(arrOfEquation[i]);
+    }
+
+    let discriminant = Math.pow(arrOfEquation[1],2) - 4 * arrOfEquation[0] * arrOfEquation[2];
+
+    let root1 = Math.round((-(arrOfEquation[1]) + Math.sqrt(discriminant)) / (2 * arrOfEquation[0]));
+    let root2 = Math.round((-(arrOfEquation[1]) - Math.sqrt(discriminant)) / (2 * arrOfEquation[0]));
+
+    if (root1 < root2) {
+        return [root1, root2];
+    } else {
+        return [root2, root1];
+    }
+
 }
